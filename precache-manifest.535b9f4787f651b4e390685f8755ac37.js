ccc7d0add469ef86a93ac618e902e3c36fff4309
Alessio <alessiofranceschi2@gmail.com>
2021-01-18 17:54:11 +0100

    Updates

---
+++ precache-manifest.535b9f4787f651b4e390685f8755ac37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e041bf4f501567a9245987d37d3097ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a6a83ab6255417d7c3a",
+    "url": "./static/css/main.3a9d72d9.chunk.css"
+  },
+  {
+    "revision": "a52fe1b9327ee664c85e",
+    "url": "./static/js/2.5be0bce0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5be0bce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6a83ab6255417d7c3a",
+    "url": "./static/js/main.cccb9421.chunk.js"
+  },
+  {
+    "revision": "3887be315370db1afffe",
+    "url": "./static/js/runtime-main.3976aae4.js"
+  }
+]);
