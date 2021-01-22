f45229fd11bc91fd7c00c2b4eb87ce6496dbf785
Alessio Franceschi <alessiofranceschi2@gmail.com>
2021-01-22 11:41:28 +0100

    Fix to build

--- src/lib/emojiEvent.js
+++ src/lib/emojiEvent.js
@@ -1,6 +1,4 @@
-import { EmojiObject } from '../types';
-
-const emojiEvent = (emoji: EmojiObject) => {
+const emojiEvent = (emoji) => {
   // eslint-disable-next-line no-undef
   const event = new Event('emoji', { bubbles: true });
   event.emoji = emoji;
